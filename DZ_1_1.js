70855ebe0fb451fda4261213846ac0e4bd0ed8a3
alenglazkov <68776957+alenglazkov@users.noreply.github.com>
2022-08-05 20:04:19 +0300

    Add files via upload

--- DZ_1_1.js
+++ DZ_1_1.js
@@ -1,8 +1,12 @@
+// ДЗ 1, часть 1
+
 let enterNumber1 = prompt("Введите первое число", "");
 if (Number(enterNumber1)) {
     let enterNumber2 = prompt("Ведите второе число", "");
-    if (Number(enterNumber2)) {enterNumber1 = +enterNumber1; enterNumber2 = +enterNumber2;
-    alert( enterNumber1.toString(enterNumber2) );}
+    if (Number(enterNumber2)) {
+        enterNumber1 = +enterNumber1; enterNumber2 = +enterNumber2;
+        console.log( enterNumber1.toString(enterNumber2) );
+    }
     else alert("Некорректный ввод!"); 
 }
 else alert("Некорректный ввод!");  
--- DZ_1_2.js
+++ DZ_1_2.js
@@ -1,10 +1,13 @@
+// ДЗ 1, часть 2
+
 let enterNumber1 = prompt("Введите первое число", "");
 if (Number(enterNumber1)) {
     let enterNumber2 = prompt("Ведите второе число", "");
-    if (Number(enterNumber2)) {enterNumber1 = +enterNumber1; enterNumber2 = +enterNumber2;
-    alert( `Ответ. Сумма двух чисел: ${enterNumber1 + enterNumber2}, 
-    Часное двух чисел: ${enterNumber1 / enterNumber2}` );} 
-    
+    if (Number(enterNumber2)) {
+        enterNumber1 = +enterNumber1; enterNumber2 = +enterNumber2;
+        console.log( `Ответ. Сумма двух чисел: ${enterNumber1 + enterNumber2}, 
+       Часное двух чисел: ${enterNumber1 / enterNumber2}` );
+        } 
     else alert("Некорректный ввод!"); 
 }
 else alert("Некорректный ввод!");  
